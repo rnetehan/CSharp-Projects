7275fb4e9c1efe68e9ca1d16fd7df3e3cc0c6967
_meteh4n_ <metehantambas4116@gmail.com>
2025-12-02 15:51:40 +0300

    KALITIM (INHERITANCE)
    English:C# inheritance study Türkçe: c# kalıtım çalışması

---
+++ kalıtım/kalıtım/Hesapla.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace kalıtım
+{
+    class Hesapla:Dortislem
+    {
+        public double fonksiyon(double a, double b)
+        {
+            return (4*(a + b))/7;
+        }
+       
+        public virtual double topla(double a, double b)
+        {
+            return 2 * (a + b);                        
+        }
+      
+
+    }
+}
---
+++ kalıtım/kalıtım/Hesapla2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace kalıtım
+{
+    class Hesapla2:Hesapla
+    {
+        public override double topla(double a, double b)
+        {
+            return 3 * (a + b);                                     
+        }
+       
+
+    }
+}
